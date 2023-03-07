0a6be0c85f71d5c0e2bad9977483736204093464
John Dietz <john@kubefirst.com>
2023-03-07 00:33:49 -0500

    chore: give preprod its own config and build space

---
+++ docusaurus.preprod.config.js
@@ -0,0 +1,114 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Kubefirst',
+  tagline: 'Instant Kubernetes Platforms',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.kubefirst.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/preprod/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Kubefirst',
+        logo: {
+          alt: 'Kubefirst website',
+          src: 'img/logo.svg',
+          href: 'https://kubefirst.io/',
+        },
+        items: [
+          /*{
+            type: 'docsVersionDropdown',
+          },*/
+          {
+            href: 'https://github.com/kubefirst/kubefirst',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Slack',
+                href: 'https://kubefirst.io/slack',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/kubefirst',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'https://kubefirst.io/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/kubefirst/kubefirst',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © 2021-${new Date().getFullYear()} Kubefirst.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
