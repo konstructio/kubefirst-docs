7a394bc535302cc6a9750297de66a74e70d10646
Frédéric Harper <hi@fred.dev>
2023-05-26 12:54:42 -0400

    feat: add code embedding from GitHub repositories (#267)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
     ],
   ],
 
-  themes: ['docusaurus-theme-search-typesense'],
+  themes: ['docusaurus-theme-search-typesense', '@saucelabs/theme-github-codeblock'],
 
   plugins: [
     [
