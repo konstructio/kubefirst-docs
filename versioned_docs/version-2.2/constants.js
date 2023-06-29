b74f1a0ebf84fe0f339242a1d5d0fd7a189dbb40
Frédéric Harper <hi@fred.dev>
2023-06-29 11:13:31 -0400

    docs: create v2.2 documentation (#343)

---
+++ versioned_docs/version-2.2/constants.js
@@ -0,0 +1,5 @@
+export const AWS_LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/512px-Amazon_Web_Services_Logo.svg.png?20170912170050";
+export const CIVO_LOGO_URL = "https://www.civo.com/assets/public/brand-assets/civo-logo-colour-60cc1622dedf346f7afde1fff760523f731b0aac106a5465af98ff4073114b74.svg";
+export const K3D_LOGO_URL = "https://k3d.io/v5.5.1/static/img/k3d_logo_black_blue.svg";
+export const GITHUB_LOGO_URL = "https://assets.kubefirst.com/console/github.svg";
+export const GITLAB_LOGO_URL = "https://assets.kubefirst.com/console/gitlab.svg";
