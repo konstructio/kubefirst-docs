f673ae70df7996bfa7fa13a5d4083ba01ae5497c
Frédéric Harper <hi@fred.dev>
2023-08-29 10:06:40 -0400

    chore: copy beta clouds docs in 2.2 (#476)

--- docs/constants.js
+++ docs/constants.js
@@ -5,3 +5,4 @@ export const GITHUB_LOGO_URL = 'https://assets.kubefirst.com/console/github.svg'
 export const GITLAB_LOGO_URL = 'https://assets.kubefirst.com/console/gitlab.svg';
 export const K3D_LOGO_URL = 'https://k3d.io/v5.5.1/static/img/k3d_logo_black_blue.svg';
 export const VULTR_LOGO_URL = 'https://cf-assets.www.cloudflare.com/slt3lc6tev37/1ATBPX4YOBbCRkybT4zS2e/f4f5f0900b57c61e65960efb3d11c64f/Vultr_logo_high_res.png';
+export const DO_LOGO_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/DigitalOcean_logo.svg/1200px-DigitalOcean_logo.svg.png';
--- versioned_docs/version-2.2/do/upgrade.md
+++
@@ -1,5 +0,0 @@
----
-title: DigitalOcean
----
-
-DigitalOcean support was added as beta in kubefirst v2.1, but the [documentation is only starting v2.3](https://docs.kubefirst.io/next/do/overview).
