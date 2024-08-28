93eb85e00bd1624757fb189d53a425aa83cc0df6
Frédéric Harper <hi@fred.dev>
2024-08-28 15:28:10 -0400

    chore: trying the docs plugin route path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
       'classic',
       {
         docs: {
-          routeBasePath: '/',
+          routeBasePath: '/docs',
           sidebarPath: require.resolve('./sidebars.js'),
           remarkPlugins: [require('remark-docusaurus-tabs')],
         },
